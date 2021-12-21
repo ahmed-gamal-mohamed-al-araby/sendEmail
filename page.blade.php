f926fc969974fcf942cd5d63626fed90d7bea8f1
ahmed-gamal-mohamed-al-araby <66698440+ahmed-gamal-mohamed-al-araby@users.noreply.github.com>
2021-12-21 09:15:46 +0200

    Add files via upload

---
+++ page.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="" style="text-align: center; margin: 250px auto;">
+        <h1>Welcome !!</h1>
+        <img src="{{asset('images/logo.png')}}" class="img-fluid" alt="">
+        <a href="{{route('login')}}" style="text-decoration: none;background-color: #17a2b8;color: #FFF;padding: 10px 20px; border-radius: 8px;">Admin Login</a>
+    </div>
+</body>
+
+</html>
